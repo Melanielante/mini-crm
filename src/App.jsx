5a10c0a94665c1fc9dfa6861a9ebb5a0d3ac2f17
Samuel <sam3bett@gmail.com>
2025-07-23 23:11:51 +0300

    create homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import  CustomerList  from './components/CustomerList';
 import  EditCustomer  from './components/EditCustomer';
 import  CustomerDetails  from './components/CustomerDetails';
 import  Navbar  from './components/Navbar';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div className="app">
       <Navbar />
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/list" element={<CustomerList />} />
         <Route path="/add" element={<AddCustomer />} />
         <Route path="/edit/:id" element={<EditCustomer />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
+
 function Navbar() {
   return (
     <nav className="navbar flex  items-center p-4 bg-gray-400">
       <div className="flex items-center justify-between w-full">
       <h1 className='font-bold'>Customer Relationship Management</h1>
       <ul className="flex space-x-4 ml-4">
-        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/home">Home</NavLink></li>
         <li><NavLink to="/add">Add Customer</NavLink></li>
         <li><NavLink to="/list">Current Customers</NavLink></li>
       </ul>
