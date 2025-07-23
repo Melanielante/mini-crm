34c9ae81f9d1af28bb98f5390639377b1a3b4265
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-07-23 12:34:55 +0300

    add routing

--- src/components/CustomerDetails.jsx
+++ src/components/CustomerDetails.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function CustomerDetails () {
+  // const {id} = useParams();
+  // const navigate = useNavigate();
+  // const BASE_URL = "https://json-server-template-w3qs.onrender.com/customers";
+
+  // const [customer, setCustomer] 
+
+
+
   return (
     <div>
         <h1>Customer Details</h1>
