cf61657c4c558f93ebf490469656f5fa9b04b6ca
Samuel <sam3bett@gmail.com>
2025-07-24 10:48:18 +0300

    create basic customer card layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,7 @@ function Navbar() {
   return (
    <div>
     <nav>
-      <img src="./react.svg" alt="logo" />
-        <h1>Customer Relationship Management</h1>
+      <h1>Customer Relationship Management</h1>
         <ul>
           <li><NavLink to={"/"}>Home</NavLink></li>
           <li><NavLink to={"/add"}>Add Customer</NavLink></li>
